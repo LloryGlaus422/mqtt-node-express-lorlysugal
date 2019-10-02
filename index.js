6296dca6a294798d0d067f70f06ddb286929b671
Yol-B <yoltorres24@gmail.com>
2019-10-02 15:22:41 +0800

    Helllllo

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
+// var app = require('express')();
+// var express = require('express');
+// var http = require('http').createServer(app);
+// var port = process.env.PORT || 3000;
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+//  });
+
+//  app.use(express.static('public'));
+
+// http.listen(port, function(){
+//   console.log('listening on *: ' + port);
+// });
+
+
 var app = require('express')();
 var express = require('express');
 var http = require('http').createServer(app);
-var path = require('path');
 var port = process.env.PORT || 3000;
 // var mqtt = require('mqtt');
-
-
-
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
  });
 
- app.use(express.static('public'));
+app.use(express.static('public'));
 
 http.listen(port, function(){
   console.log('listening on *: ' + port);
